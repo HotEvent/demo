53cf3afa9b15c4bb6c3f11aa7e18c61adc96fd88
HotEvent <1807657825@qq.com>
2021-01-07 22:49:15 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FireflyCss/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FireflyCss from './components/FireflyCss.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FireflyCss
   }
 }
 </script>
@@ -24,5 +23,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  min-height: 100vh;
+  background: black;
 }
 </style>
